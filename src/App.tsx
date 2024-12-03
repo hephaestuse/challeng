7b0ad0f09c64f4cc7f45c43cf0b1257650054c4a
ahad-mohammadi <mohammadiahad93@gmail.com>
2024-12-03 18:31:32 +0330

    bg Img format change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Form from "./features/form/Form";
 
 export default function Home() {
   return (
-    <div className="bg-[url('BgLogin.png')] bg-no-repeat bg-cover w-screen h-screen flex justify-center items-center">
+    <div className="bg-[url('BgLogin.webp')] bg-no-repeat bg-cover w-screen h-screen flex justify-center items-center">
       <Form />
     </div>
   );
